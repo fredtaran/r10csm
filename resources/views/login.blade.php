a8522a6e6b8702032bd701a50917b75764832d25
fredtaran <ftaran04@gmail.com>
2023-12-14 18:00:21 +0800

    Login Form

---
+++ resources/views/login.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CSM | Login</title>
+        <link rel="stylesheet" href="{{ asset('css/login_style.css') }}">
+    </head>
+    <body>
+        <div class="login-main-wrapper">
+            
+        </div>
+    </body>
+</html>
